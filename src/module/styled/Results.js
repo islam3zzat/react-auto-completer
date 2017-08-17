1dbac37cf92509ace524bb637bf5148b44b34b58
islam3zzat@gmail.com <islam3zzat@gmail.com>
2017-08-17 22:13:05 +0200

    create app styled components

---
+++ src/module/styled/Results.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export default styled.ul`
+    position: absolute;
+    list-style-type: none;
+    margin-top: 0;
+    max-height: 200px;
+    min-width: 160px;
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 0 20px 0 20px;
+    border-radius: 3px;
+    overflow: auto;
+    color: white;
+`
