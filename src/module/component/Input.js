e10f6b54d1ed301c8ba3e0b3b222007ecc4dcb14
islam3zzat@gmail.com <islam3zzat@gmail.com>
2017-08-19 11:46:35 +0200

    activate component on input active

--- src/module/component/Input.js
+++ src/module/component/Input.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Input from '../styled/Input'
 
-export default ({value, changed, placeholder}) => (
+export default ({value, changed, placeholder, active, setActive}) => (
     <Input
+        onFocus={()=>{setActive(!active)}}
         className="react-autocomplete__input"
         type="text"
         onChange={(e)=>{changed(e.target.value)}}
