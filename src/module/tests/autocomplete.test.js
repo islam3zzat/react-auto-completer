046126e3d41bb2a650a63fa0de0ff1c1f074d499
islam3zzat@gmail.com <islam3zzat@gmail.com>
2017-08-21 23:13:26 +0200

    add test for rendering default placeholder

--- src/module/tests/autocomplete.test.js
+++ src/module/tests/autocomplete.test.js
@@ -17,3 +17,11 @@ it('renders Autocomplete without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Autocomplete />, div);
 });
+
+describe('user provided props', () => {
+  it('should render default placeholder', () => {
+    let autoComplete = <Autocomplete />
+    let renderedAutocomplete = render(autoComplete)
+    let input = renderedAutocomplete.find('input')
+    expect(input.attr('placeholder')).toBe('Placeholder')
+  })
