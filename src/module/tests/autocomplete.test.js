f124a414a3ef3aefa3868b4cbfbc6c7f6e83a146
islam3zzat@gmail.com <islam3zzat@gmail.com>
2017-08-21 23:13:51 +0200

    add test for user provided placeholder

--- src/module/tests/autocomplete.test.js
+++ src/module/tests/autocomplete.test.js
@@ -25,3 +25,10 @@ describe('user provided props', () => {
     let input = renderedAutocomplete.find('input')
     expect(input.attr('placeholder')).toBe('Placeholder')
   })
+  it('should render provided placeholder', () => {
+    let autoComplete = <Autocomplete placeholder="search please"/>
+    let renderedAutocomplete = render(autoComplete)
+    let input = renderedAutocomplete.find('input')
+    expect(input.attr('placeholder')).toBe('search please')
+  })
+})
