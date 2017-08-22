eb4f12f8d901a89bed24512849ac7a4f7777b3ca
islam3zzat@gmail.com <islam3zzat@gmail.com>
2017-08-22 22:10:26 +0200

    reformat file

--- src/module/component/Input.js
+++ src/module/component/Input.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Input from '../styled/Input'
 
 export default ({value, changed, placeholder, active, setActive}) => (
-    <Input
-        onFocus={()=>{setActive(!active)}}
-        className="react-autocomplete__input"
-        type="text"
-        onChange={(e)=>{changed(e.target.value)}}
-        value={value}
-        placeholder={placeholder}
-    />
+  <Input
+    onFocus={()=>{setActive(true)}}
+    className="react-autocomplete__input"
+    type="text"
+    onChange={(e)=>{changed(e.target.value)}}
+    value={value}
+    placeholder={placeholder}
+  />
 )
