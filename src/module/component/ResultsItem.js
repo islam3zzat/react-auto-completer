e3f394de6c9b236de40bf672dc0a8ddcdffeb6c0
islam3zzat@gmail.com <islam3zzat@gmail.com>
2017-08-23 22:35:43 +0200

    add result item tab index

--- src/module/component/ResultsItem.js
+++ src/module/component/ResultsItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ResultItem from '../styled/ResultsItem'
 
-export default ({data, select}) => {
-  return <ResultItem onClick={()=>{select(data)}}>{data}</ResultItem>
+export default ({data, select, tabIndex}) => {
+  return <ResultItem tabIndex={tabIndex} onClick={()=>{select(data)}}>{data}</ResultItem>
 }
