b7625f8c299d666a0fb93fb3d391280856596d2f
islam3zzat@gmail.com <islam3zzat@gmail.com>
2017-08-23 22:40:04 +0200

    selects element if enter was clicked

--- src/module/component/ResultsItem.js
+++ src/module/component/ResultsItem.js
@@ -2,5 +2,5 @@ import React from 'react'
 import ResultItem from '../styled/ResultsItem'
 
 export default ({data, select, tabIndex}) => {
-  return <ResultItem tabIndex={tabIndex} onClick={()=>{select(data)}}>{data}</ResultItem>
+  return <ResultItem  onKeyPress={(e)=>{if(e.key == 'Enter') select(data)}}  tabIndex={tabIndex} onClick={()=>{select(data)}}>{data}</ResultItem>
 }
