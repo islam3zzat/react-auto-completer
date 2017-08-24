51b7ef329561893f9e1b0bb1604175987309ee40
islam3zzat@gmail.com <islam3zzat@gmail.com>
2017-08-24 22:31:55 +0200

    focus on next element on pressing down

--- src/module/component/ResultsItem.js
+++ src/module/component/ResultsItem.js
@@ -4,6 +4,11 @@ import ResultItem from '../styled/ResultsItem'
 export default ({data, select, tabIndex}) => {
   return <ResultItem
     onKeyDown={(e)=>{
+      if (e.key === 'ArrowDown') {
+        e.preventDefault()
+        if(e.target.nextElementSibling){
+          e.target.nextElementSibling.focus()
+        }
     }}
     onKeyPress={(e)=>{
       if(e.key === 'Enter') select(data)}
