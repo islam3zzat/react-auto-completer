b29981f6462a8b5bb51ac88ac307810faffa122b
islam3zzat@gmail.com <islam3zzat@gmail.com>
2017-08-24 22:30:59 +0200

    reformat component

--- src/module/component/ResultsItem.js
+++ src/module/component/ResultsItem.js
@@ -3,6 +3,9 @@ import ResultItem from '../styled/ResultsItem'
 
 export default ({data, select, tabIndex}) => {
   return <ResultItem
+    onKeyPress={(e)=>{
+      if(e.key === 'Enter') select(data)}
+    }
     tabIndex={tabIndex}
     onClick={()=>{select(data)}}>{data}</ResultItem>
 }
