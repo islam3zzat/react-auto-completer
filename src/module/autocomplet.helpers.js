22ed7f03f531aafbcb6767a1be77caa4aa3f0efa
islam3zzat@gmail.com <islam3zzat@gmail.com>
2017-08-25 22:58:42 +0200

    pass additional params

--- src/module/autocomplet.helpers.js
+++ src/module/autocomplet.helpers.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
-export function fetchApi(options){
+export function fetchApi(options, additionalParams){
   // if no search key is passed, append value
   if (!options.searchKey) {
     options.url += `/${options.val}`
   }
   return axios({
     ...options,
-    params: {[options.searchKey]: options.val},
-    data: {[options.searchKey]: options.val}
+    params: {...{[options.searchKey]: options.val} , ...additionalParams},
+    data: {...{[options.searchKey]: options.val}, ...additionalParams}
   })
 }
