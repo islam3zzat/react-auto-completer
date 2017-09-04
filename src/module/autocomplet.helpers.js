c31781b3c8b6550e59960dad300d20635210b6d7
islam3zzat@gmail.com <islam3zzat@gmail.com>
2017-09-04 22:25:48 +0200

    cancel any previouse request

--- src/module/autocomplet.helpers.js
+++ src/module/autocomplet.helpers.js
@@ -1,14 +1,21 @@
 import axios from 'axios'
 
+let cancel;
 export function fetchApi(options, additionalParams, additionalHeaders){
   // if no search key is passed, append value
   if (!options.searchKey) {
     options.url += `/${options.val}`
   }
+  if (cancel) {
+    cancel();
+  }
   return axios({
     ...options,
     params: {...{[options.searchKey]: options.val} , ...additionalParams},
     data: {...{[options.searchKey]: options.val}, ...additionalParams},
-    headers: {...additionalHeaders}
+    headers: {...additionalHeaders},
+    cancelToken: new axios.CancelToken(function executor(c) {
+      cancel = c;
+    })
   })
 }
